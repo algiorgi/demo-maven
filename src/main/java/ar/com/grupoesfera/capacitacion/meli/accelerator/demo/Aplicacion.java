54178e652d2a9f32ca1b159e23240e60d4e34630
geagiorgi <alejandro.giorgi@grupoesfera.com.ar>
2020-12-10 12:20:46 -0300

    Commit inicial

---
+++ src/main/java/ar/com/grupoesfera/capacitacion/meli/accelerator/demo/Aplicacion.java
@@ -0,0 +1,8 @@
+package ar.com.grupoesfera.capacitacion.meli.accelerator.demo;
+
+public class Aplicacion {
+
+    public static void main(String[] args) {
+        System.out.println("Hola desde maven");
+    }
+}
---
+++ src/test/java/ar/com/grupesfera/capacitacion/meli/accelerator/unit/MiPrimerTest.java
@@ -0,0 +1,12 @@
+package ar.com.grupesfera.capacitacion.meli.accelerator.unit;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class MiPrimerTest {
+
+    @Test
+    public void miPrimeraPrueba() {
+        Assert.assertTrue(true);
+    }
+}
